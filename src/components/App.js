ce1aa52266193b0888ceb196eb207ebeea435025
unknown <bilalelbedoui93@gmail.com>
2019-10-08 20:46:31 +0200

    first commit- webpack has been set up

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+//import { Router, Route, Switch, Redirect } from 'react-router-dom'
+
+
+const App = () => {
+
+    return (
+        <div>
+            
+            {console.log('rendering <App>...')}
+
+
+            <h1>ESTA VA A SER MI APP</h1>
+
+
+        </div>
+    )
+
+
+}
+export default App;
